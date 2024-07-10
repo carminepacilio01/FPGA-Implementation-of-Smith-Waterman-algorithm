a7785ffaa72ec710e5b98589c0025874f7777dbb
carminepacilio01 <carmine.pacilio@mail.polimi.it>
2024-07-11 00:43:01 +0200

    Modified header of design

--- Design/smith_waterman.hpp
+++ Design/smith_waterman.hpp
@@ -6,8 +6,6 @@
 #include <vector>
 #include <limits>
 
-
-
 #define MAX_DIM 1024
 #define MAX_REP MAX_DIM * 2
 #define MATRIX_SIZE MAX_DIM * MAX_DIM
@@ -26,8 +24,8 @@ typedef struct conf {
 	int gap_extension;
 } conf_t;
 
-void computeSW(int lenT, char *target, int lenD, char *database, conf_t scoring, int *score);
 void sw_maxi(int lenT[INPUT_SIZE], char target[INPUT_SIZE][MAX_DIM], int lenD[INPUT_SIZE], char database[INPUT_SIZE][MAX_DIM], int wd, int ws, int gap_opening, int enlargement, int score[INPUT_SIZE], int input_len);
+void computeSW(int lenT, char *target, int lenD, char *database, conf_t scoring, int *score);
 void compute_diag(int num_diag, int lenT, int &diag_len, int max_diag_len, int &database_cursor, int &target_cursor, int &n_diag_repeat, int &diag_index, char target_l[MAX_DIM], char database_l[MAX_DIM], conf_t scoring, int p_buffer[3][MAX_DIM * 2], int q_buffer[3][MAX_DIM * 2], int d_buffer[3][MAX_DIM * 2], int &score_l);
 void readInput(int lenT[MAX_DIM], int lenT_local[MAX_DIM], char target[INPUT_SIZE][MAX_DIM], char t_local[INPUT_SIZE][MAX_DIM], int lenD[MAX_DIM], int lenD_local[MAX_DIM], char database[INPUT_SIZE][MAX_DIM], char db_local[INPUT_SIZE][MAX_DIM]);
 void writeOutput(int score_l[INPUT_SIZE], int score[INPUT_SIZE]);
